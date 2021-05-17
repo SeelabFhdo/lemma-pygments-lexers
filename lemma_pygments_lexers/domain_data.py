41142f3e5d26773b0ed6641d72125459ac9473de
Florian Rademacher <florian.rademacher@fh-dortmund.de>
2021-05-17 14:08:03 +0200

    Domain Data Lexer: Fix recognition of feature syntax

--- lemma_pygments_lexers/domain_data.py
+++ lemma_pygments_lexers/domain_data.py
@@ -23,7 +23,14 @@ class LemmaDomainDataLexer(RegexLexer):
                 'unspecified'
             ), prefix=r'\b', suffix=r'\b'), Keyword),
             (r'(true|false)\b', Keyword.Constant),
-            (r'(<)(\w+)(>)', bygroups(Text, Name.Builtin.Pseudo, Text)),
+            # Features with one or two components, e.g., "<aggregate>" or
+            # "<aggregate, entity>"
+            (r'(<)(?:(\w+)(,))?(\s*)(\w+)(>)', bygroups(Text,
+                Name.Builtin.Pseudo, Text, Text, Name.Builtin.Pseudo, Text)),
+            # Features with three components, e.g., "<aggregate,entity,factory>"
+            (r'(<)(\w+)(,)(\s*)(\w+)(,)(\s*)(\w+)(>)', bygroups(Text,
+                Name.Builtin.Pseudo, Text, Text, Name.Builtin.Pseudo, Text,
+                Text, Name.Builtin.Pseudo, Text)),
             (r'\".*?\"', String)
         ]
     }
